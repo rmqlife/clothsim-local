fe699c131a59ad32e7209a1e99137e06474efedd
rmqlife <rmqlife@gmail.com>
2018-02-01 14:54:38 -0500

    init

---
+++ process.sh
@@ -0,0 +1,11 @@
+D="result02-rand"
+D2="fixed6"
+python merge_dataset.py $D $D2
+mv $D/*.py $D2
+cd $D2
+mkdir depth_sim
+mv png/*depth*.png depth_sim
+python ../add_bg.py depth_sim depth_sim_bg ../bg.png
+python ../build_depth_feature.py depth_sim_bg depth_sim_bg
+cd ..
+rm -r $D
